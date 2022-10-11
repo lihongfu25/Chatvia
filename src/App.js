0b272c53d1b2845ebca885a7c936b9d4e2522e4b
lihongfu25 <phuhd2k1@gmail.com>
2022-10-11 22:14:06 +0700

    [Refactor]: add mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { routes } from "./routes";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
@@ -9,7 +10,8 @@ library.add(far, fas);
 
 function App() {
     const appRoutes = useRoutes(routes);
-    return <div>{appRoutes}</div>;
+    const currentMode = useSelector((state) => state.mode.currentMode);
+    return <div className={currentMode}>{appRoutes}</div>;
 }
 
 export default App;
